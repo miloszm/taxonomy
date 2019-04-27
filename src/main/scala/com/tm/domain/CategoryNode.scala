fffbdf6024fcbb5fbd7132cc2f56712f255eae85
miloszm <milosz.muszynski@digital.hmrc.gov.uk>
2019-04-27 18:42:01 +0100

    refactored implementation with findFirs and findAll

--- src/main/scala/com/tm/domain/CategoryNode.scala
+++ src/main/scala/com/tm/domain/CategoryNode.scala
@@ -5,13 +5,20 @@ import scala.annotation.tailrec
 case class Id(id: String)
 
 case class CategoryNode(id: Id, name: String, children: Seq[CategoryNode], tags: Seq[TagId] = Nil){
-  def getNode(id: Id): Option[CategoryNode] = {
-    if (this.id == id) Some(this)
+
+  def getNode(id: Id): Option[CategoryNode] = findFirst(_.id == id)
+
+  def getDescendants: Seq[CategoryNode] = children.flatMap(_.findAll(_ => true))
+
+  def getNodesWithTag(tagId: TagId): Seq[CategoryNode] = findAll(_.tags.exists(_ == tagId))
+
+  def findFirst(f: CategoryNode => Boolean): Option[CategoryNode] = {
+    if (f(this)) Some(this)
     else {
       @tailrec
       def go(nodes: Seq[CategoryNode]): Option[CategoryNode] = nodes match {
         case Nil => None
-        case x::xs => x.getNode(id) match {
+        case x::xs => x.findFirst(f) match {
           case found@Some(_) => found
           case None => go(xs)
         }
@@ -19,8 +26,9 @@ case class CategoryNode(id: Id, name: String, children: Seq[CategoryNode], tags:
       go(children)
     }
   }
-  def getDescendants: Seq[CategoryNode] = children ++ children.flatMap(_.getDescendants)
-  def getNodesWithTag(tagId: TagId): Seq[CategoryNode] =
-    List(this).filter(_.tags.exists(_ == tagId)) ++ children.flatMap(_.getNodesWithTag(tagId))
+
+  def findAll(f: CategoryNode => Boolean): Seq[CategoryNode] = {
+    List(this).filter(f) ++ children.flatMap(_.findAll(f))
+  }
 }
 
